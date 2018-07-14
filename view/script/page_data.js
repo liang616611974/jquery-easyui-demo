b25c38baa0e16f81231bb7ba152bc32973024aaa
liangfeng <616611974@qq.com>
2018-07-14 18:28:48 +0800

    1.添加了接下来的开发任务说明

--- view/script/page_data.js
+++ view/script/page_data.js
@@ -5,3 +5,12 @@ page.dict.int = {}
 page.dict.fucn = {}
 page.dict.action = {};
 page.dict.obj = {};
+
+/*
+todo
+1.完成按钮事件驱动
+    1.1 使用form Plugins 工具完成上传图片和下载功能
+2.完成表格组装
+3.页面action由应该结合功能命名
+4.完善页面，去除插件元素耦合
+ */
